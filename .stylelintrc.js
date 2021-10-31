9776ef17d194bdd73d757566f05194816d4050d3
Nikita Butenko <nik@butenko.me>
2021-10-31 14:36:52 +1100

    Remove stylelint

--- .stylelintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [require.resolve('stylelint-config-prettier')],
-  plugins: [require.resolve('stylelint-prettier')],
-  rules: {
-    'prettier/prettier': true
-  }
-};
